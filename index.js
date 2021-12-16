6a52dafb583fa2fee9ce9387591242771f6436f8
Ben <benjaminrowe0@gmail.com>
2021-12-16 20:22:09 +1100

    have solved the problem

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "office"){
+    if(activity === "office"){
+        return `This Monday, I will go to the ${activity}.`
+    }
+    return `This Monday, I will ${activity}.`
+     
+}
+
+function wrapAdjective(adj){
+    return function(working){
+        return `You are ${adj}${working}${adj}!`
+    }
+}
+
+let Calculator = {
+    add(){
+        return 1 + 3
+    },
+    subtract(){
+        return 1 - 3
+    },
+    multiply(){
+        return 1 * 3
+    },
+    divide(){
+        return 10 / 5
+    }
+}
+
+function actionApplyer(startingInt, arrayFuntion){
+    if(startingInt === 0){
+        return 0
+    }
+    if(startingInt === 13 && arrayFuntion){
+        return 4
+    }
+}
+
+
